1bf5151a54bc801b22f56731d26ec5cb059563df
hisyam210 <hisyam210302@gmail.com>
2023-11-21 09:26:16 +0700

    menambahakan list baru

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello wordl</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, optio repellendus perspiciatis saepe impedit nemo unde ullam eos molestias ipsam ducimus quos, eveniet, alias minus? Praesentium dolore debitis quidem aperiam.</p>
+    <ul>
+        <li>php</li>
+        <li>html</li>
+        <li>css</li>
+    </ul>
 </body>
 </html>
